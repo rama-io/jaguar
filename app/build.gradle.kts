8b93407696c957d812ff10a07a05aaf972fcf20b
Pombo <github.5yeil@simplelogin.fr>
2026-08-09 14:53:14 -0700

    removing mono themes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.rama.jaguar"
         minSdk = 21
         targetSdk = 37
-        versionCode = 1
+        versionCode = 2
         versionName = "$currentYear.$versionCode"
     }
 
--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
 # Changelog
 
+## 2
+
+- Removing mono themes (refactoring these themes would have to happen globally in the organization)
+
 ## 1
